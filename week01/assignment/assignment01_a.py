157cbf3b73194f4131988d3e825bbc715ffa0d5c
Jacob Running <86081584+Twister1M@users.noreply.github.com>
2023-01-09 13:03:06 -0500

    01.a

--- week01/assignment/assignment01_a.py
+++ week01/assignment/assignment01_a.py
@@ -1,10 +1,10 @@
 '''
 Requirements:
 1. Write a function that takes a number and computes the sum of all numbers between
-   one and that number (exclusive). This will be the target of your thread.
+    one and that number (exclusive). This will be the target of your thread.
 2. Create a thread to run this function.
 3. Assert that your sums are correct for the given number.
-   
+
 Psuedocode:
 1. Create either a global SUM or create a list object in main.
 2a. If using a global, then inside of your function, set the global equal to the sum.
@@ -13,10 +13,9 @@ Psuedocode:
 4. Using assert, check the expected result (see main)
 5. Repeat steps 3 and 4, but use 13.
 6. Repeat steps 3 and 4, but use 17.
-
 Things to consider:
 a. If using a global, what is the correct syntax for creating a thread with one argument?
-   (see https://stackoverflow.com/questions/3221655/python-threading-string-arguments)
+    (see https://stackoverflow.com/questions/3221655/python-threading-string-arguments)
 b. How do you start a thread? (see this week's reading) 
 c. How will you wait until the thread is done? (see this week's reading)
 d. Do threads (including the main thread) share global variables? (see https://superfastpython.com/thread-share-variables/)
@@ -31,22 +30,41 @@ import threading
 # global sum
 SUM = 0
 
+def sum_func(number):
+   
+   global SUM
+   
+   cur_sum = 0
+   
+   for val in range(1,number):
+   
+      cur_sum += val
+   
+   SUM = cur_sum
+
+
+
 def main():
    
-   # If not using a global, use this list to store your results
-    results = [0] * 3
+   t1 = threading.Thread(target=sum_func, args=(10,))
+   t1.start()
+   t1.join()
    
-   # If using a global, place these asserts after the call to your summing function 
-   # to check that your code is working correctly
-   #assert SUM == 45, f'The sum should equal 45 but instead was {SUM}'  
-   #assert SUM == 78, f'The sum should equal 78 but instead was {SUM}'    
-   #assert SUM == 136, f'The sum should equal 136 but instead was {SUM}'
+   assert SUM == 45, f'The sum should equal 45 but instead was {SUM}'  
    
-   # If using a list object, place these asserts after the call to your summing function
-   # to check that your code is working correctly
-   #assert results[0] == 45, f'The sum should equal 45 but instead was {results[0]}'  
-   #assert results[1] == 78, f'The sum should equal 78 but instead was {results[1]}'    
-   #assert results[2] == 136, f'The sum should equal 136 but instead was {results[2]}'
+   t2 = threading.Thread(target=sum_func, args=(13,))
+   t2.start()
+   t2.join()
+   
+   assert SUM == 78, f'The sum should equal 78 but instead was {SUM}'  
+   
+   t3 = threading.Thread(target=sum_func, args=(17,))
+   t3.start()
+   t3.join()
+   
+   assert SUM == 136, f'The sum should equal 136 but instead was {SUM}'
+
+
 
 if __name__ == '__main__':
    main()
